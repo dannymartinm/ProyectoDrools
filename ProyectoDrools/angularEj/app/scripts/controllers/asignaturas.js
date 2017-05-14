f34cdf2f0ee11a65d09ecfc06fa4c696f4a427c6
Daniela Martin <daniela@esporaestudio.com>
2017-05-14 18:37:56 -0500

    Se agrego la funcionalidad en agular para agregar una AlumnoAsignatura

--- ProyectoDrools/angularEj/app/scripts/controllers/asignaturas.js
+++ ProyectoDrools/angularEj/app/scripts/controllers/asignaturas.js
@@ -8,11 +8,31 @@
  * Controller of the angularEjApp
  */
 angular.module('angularEjApp')
-  .controller('AsignaturasCtrl', function ($http, $scope) {
+  .controller('AsignaturasCtrl', ['$http', '$scope',function ($http, $scope, $log) {
 
    $http.get('http://localhost:8080/api/asignaturas')
         .then(function(response){
           var listado = response.data;
           $scope.asignaturas = listado;
         });
-  });
+
+
+  	$scope.insertarAsignaturaAlumno = function(nombre){
+  		var matricula = $http.get('http://localhost:8080/api/alumnos')
+        .then(function(response){
+          response.data[0].matricula;
+          console.log(response.data[0].matricula);
+          //$log.debug("matricula"+res.nombre);
+        });
+  		var materiaInsertar = {
+  			"nombre": nombre,
+  			"matricula": matricula
+  		}
+  		console.log(nombre);
+  	/*	$http.post('http://localhost:8080/alumnos', alumnoInsertar)
+  		.then(function(response){
+  			var respuesta = response.data;
+  			$scope.resultadoInsercion =  respuesta;
+  		});*/
+  	}
+  }]);
