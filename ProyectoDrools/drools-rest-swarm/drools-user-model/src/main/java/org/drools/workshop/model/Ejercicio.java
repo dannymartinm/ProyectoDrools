9ba440195c47b45d11aa195ff05253203d70b6a3
Alan Herrera <alanhsca9@gmail.com>
2017-05-14 15:49:23 -0500

    Clase Ejercicio.java

---
+++ ProyectoDrools/drools-rest-swarm/drools-user-model/src/main/java/org/drools/workshop/model/Ejercicio.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.drools.workshop.model;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author 
+ */
+@XmlRootElement
+public class Ejercicio {
+	public String nombre;
+	public String descripcion;
+	private String id_ejercicio;
+	public int dificultad;
+	
+	public Ejercicio() {
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public String getId_ejercicio() {
+		return id_ejercicio;
+	}
+
+	public void setId_ejercicio(String id_ejercicio) {
+		this.id_ejercicio = id_ejercicio;
+	}
+
+	public int getDificultad() {
+		return dificultad;
+	}
+
+	public void setDificultad(int dificultad) {
+		this.dificultad = dificultad;
+	}
+
+	public Ejercicio(String nombre, String descripcion, String id_ejercicio, int dificultad) {
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+		this.id_ejercicio = id_ejercicio;
+		this.dificultad = dificultad;
+	}
+	
+	@Override
+    public String toString() {
+        return "Ejercicio{" + "nombre=" + nombre + 
+        ", descripcion=" + descripcion + 
+        ", id_ejercicio=" + id_ejercicio +
+        ", dificultad=" + dificultad +  
+        "}";
+    }
+
+}
