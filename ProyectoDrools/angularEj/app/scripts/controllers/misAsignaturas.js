89dca8731142148553c0fc45457627c1176d49dd
Itzi Reyes <itzireyes11@gmail.com>
2017-05-15 10:28:07 -0500

    Modificación en controller y view misAsignaturas

--- ProyectoDrools/angularEj/app/scripts/controllers/misAsignaturas.js
+++ ProyectoDrools/angularEj/app/scripts/controllers/misAsignaturas.js
@@ -10,32 +10,10 @@
 angular.module('angularEjApp')
   .controller('MisAsignaturasCtrl', ['$http', '$scope',function ($http, $scope, $log) {
 
-   $http.get('http://localhost:8080/api/ejercicios')
+   $http.get('http://localhost:8080/api/alumnoAsignaturas')
         .then(function(response){
           var listado = response.data;
           $scope.ejercicios = listado;
         });
 
-
-  	$scope.insertarAsignaturaAlumno = function(nombre){
-  		var matricula = $http.get('http://localhost:8080/api/alumnos')
-        .then(function(response){
-          var res = response.data[0].matricula;
-          console.log(response.data[0].matricula);
-          //$log.debug("matricula"+res.nombre);
-          var materiaInsertar = {
-  			"nombre": nombre,
-  			"promedio": promedio
-  		}
-  		console.log(nombre);
-
-  		$http.post('http://localhost:8080/api/alumnos/insertaAlumnoAsignatura', materiaInsertar)
-  		.then(function(response){
-  			var respuesta = response.data;
-  			$scope.resultadoInsercion =  respuesta;
-  		});
-        });
-        //var res = '\"'+matricula+'\"'
-  		
-  	}
   }]);
