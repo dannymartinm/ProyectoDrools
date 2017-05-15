a7b1c27f8652193fd09ca3d3b5553f5f7fbb3254
Daniela Martin <daniela@esporaestudio.com>
2017-05-14 19:45:27 -0500

    YA se puede hacer POST de AlumnoAsignatura

--- ProyectoDrools/angularEj/app/scripts/controllers/asignaturas.js
+++ ProyectoDrools/angularEj/app/scripts/controllers/asignaturas.js
@@ -20,20 +20,22 @@ angular.module('angularEjApp')
   	$scope.insertarAsignaturaAlumno = function(nombre){
   		var matricula = $http.get('http://localhost:8080/api/alumnos')
         .then(function(response){
-          response.data[0].matricula;
+          var res = response.data[0].matricula;
           console.log(response.data[0].matricula);
           //$log.debug("matricula"+res.nombre);
-        });
-  		var materiaInsertar = {
+          var materiaInsertar = {
   			"nombreAsignatura": nombre,
-  			"matricula": matricula
+  			"matricula": res
   		}
   		console.log(nombre);
 
-  		$http.post('http://localhost:8080/alumnos/insertaAlumnoAsignatura', materiaInsertar)
+  		$http.post('http://localhost:8080/api/alumnos/insertaAlumnoAsignatura', materiaInsertar)
   		.then(function(response){
   			var respuesta = response.data;
   			$scope.resultadoInsercion =  respuesta;
   		});
+        });
+        //var res = '\"'+matricula+'\"'
+  		
   	}
   }]);
